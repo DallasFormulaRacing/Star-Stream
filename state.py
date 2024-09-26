426cc4051603574d8700c5c80469460ce2f0bba2
Arjun Nayak <squidtoon99@gmail.com>
2024-09-26 03:47:42 -0500

    State management in a class for future loki implementations

--- state.py
+++ state.py
@@ -1,4 +1,19 @@
+import asyncpg
+from os import getenv
 
 class State(object):
+    conn: asyncpg.Connection 
+    
+    def __init__(self, pg_uri: str):
+        self.pg_uri = pg_uri
 
-    def __init__(self):
+        self.conn = None 
+    
+    def set_conn(self, conn: asyncpg.Connection):
+        self.conn = conn 
+            
+    async def setup(self):
+        # connect to postgres
+        self.conn = await asyncpg.connect(self.pg_uri)
+    
+    
